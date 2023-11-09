5142c9c9699673140ff75986f2dfb41613a98fc3
Bo Nicolajsen <>
2023-11-09 10:46:23 +0100

    start
    structure

---
+++ assets/js/particles.js
@@ -0,0 +1,25 @@
+
+const myCanvas = document.getElementById('myCanvas');
+const ctx = myCanvas.getContext('2d');
+
+myCanvas.width = window.innerWidth;
+myCanvas.height = window.innerHeight;
+
+console.log(ctx);
+
+class controller {
+
+    constructor (options) {
+
+    }
+
+
+}
+
+class particle {
+    constructor (options) {
+
+    }
+
+
+}
